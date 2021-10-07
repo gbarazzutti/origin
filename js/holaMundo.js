0a29fe4ef1d6c96e0babde1858eacb1e67c1cb83
Guillermo <gbarazzutti2@gmail.com>
2021-10-07 19:40:55 -0300

    practica

---
+++ js/holaMundo.js
@@ -0,0 +1,28 @@
+// Esto es una linea de comentario simple o de una sola linea
+
+/* esto es un comentario
+de multiples 
+lineas*/
+
+// mostrar un mensaje en consola
+console.log("Hola comision 16i")
+
+// mostrar msj en el html
+document.write("Este es mi primer mensaje")
+
+// mostrar un mensaje en una ventana emergente
+// alert("Hola mundo")
+
+// crear una variable (var - let - cons)
+
+let comision = "16i";
+
+document.write("<br>Comision: "+comision)
+
+// modificar el contenido de una variable
+comision = "i"
+
+document.write("<br>Comision: "+comision)
+
+const anio = 2021
+document.write("<br>Año: "+anio)
