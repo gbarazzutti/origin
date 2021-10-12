e17a5907cca21a6f8bbdf28eb4e930b2a2727442
Guillermo <gbarazzutti2@gmail.com>
2021-10-12 18:54:12 -0300

    se agrego el ejercicio 5

--- js/practica.js
+++ js/practica.js
@@ -1,2 +1,8 @@
-alert("Buenos dias")
+let numero1 = parseInt(prompt("Ingrese un primer numero"));
+let numero2 = parseInt(prompt("Ingrese un segundo numero"));
 
+// parseInt("numero")
+
+document.write(numero1+ numero2)
+console.log(numero1)
+console.log(numero2)
